183d30f55c9b641bf420f6a817a09c410d120d4a
Nilay Jain <31623181+nilayjain12@users.noreply.github.com>
2023-11-28 23:28:35 -0800

    Update test.py

--- test.py
+++ test.py
@@ -1,90 +1,28 @@
-import unittest
-import io
-import sys
-from unittest.mock import patch
+# test.py
+from main import two_sum, length_of_last_word, search_insert
 
-from people import Person
-from people import Faculty
-from people import Student
+# Test cases for two_sum function
+def test_two_sum():
+    assert two_sum([2, 7, 11, 15], 9) == [0, 1], "Test Case 1 Failed"
+    assert two_sum([3, 2, 4], 6) == [1, 2], "Test Case 2 Failed"
+    assert two_sum([3, 3], 6) == [0, 1], "Test Case 3 Failed"
+    print("All two_sum test cases passed!")
 
+# Test cases for length_of_last_word function
+def test_length_of_last_word():
+    assert length_of_last_word("Hello World") == 5, "Test Case 1 Failed"
+    assert length_of_last_word("   fly me   to   the moon  ") == 4, "Test Case 2 Failed"
+    assert length_of_last_word("luffy is still joyboy") == 6, "Test Case 3 Failed"
+    print("All length_of_last_word test cases passed!")
 
-class Test01_Person(unittest.TestCase):
-    def test_list_int(self):
-        """
-        *** Test01 *** FUNCTION CALL: x = Person('Stephen','May') *** EXPECT: x.firstname+':'+x.lastname == 'Stephen:May' ***
-        """
-        x = Person('Stephen','May')
-        self.assertEqual('Stephen:May', x.firstname+':'+x.lastname)
-        print()
+# Test cases for search_insert function
+def test_search_insert():
+    assert search_insert([1, 3, 5, 6], 5) == 2, "Test Case 1 Failed"
+    assert search_insert([1, 3, 5, 6], 2) == 1, "Test Case 2 Failed"
+    assert search_insert([1, 3, 5, 6], 7) == 4, "Test Case 3 Failed"
+    print("All search_insert test cases passed!")
 
-class Test02_Faculty(unittest.TestCase):
-    def test_list_int(self):
-        """
-        *** Test02 *** FUNCTION CALL: x = Faculty('Stephen','May','Computer Science') *** EXPECT: x.firstname+':'+x.lastname+':'+x.department == 'Stephen:May:Computer Science' ***
-        """
-        x = Faculty('Stephen','May','Computer Science')
-        self.assertEqual('Stephen:May:Computer Science', x.firstname+':'+x.lastname+':'+x.department)
-        print()
-
-class Test03_FacultyInheritsPerson(unittest.TestCase):
-    def test_list_int(self):
-        """
-        *** Test03 *** FUNCTION CALL: x = Faculty('Stephen','May','Computer Science') *** EXPECT: isinstance(x, Person) == True ***
-        """
-        x = Faculty('Stephen','May','Computer Science')
-        self.assertEqual(True, isinstance(x, Person))
-        print()
-
-class Test04_FacultyInheritsStudent(unittest.TestCase):
-    def test_list_int(self):
-        """
-        *** Test04 *** FUNCTION CALL: x = Faculty('Stephen','May','Computer Science') *** EXPECT: isinstance(x, Student) == False ***
-        """
-        x = Faculty('Stephen','May','Computer Science')
-        self.assertEqual(False, isinstance(x, Student))
-        print()
-
-class Test05_Student(unittest.TestCase):
-    def test_list_int(self):
-        """
-        *** Test05 *** FUNCTION CALL: x = Faculty('Stephen','May','Computer Science') THEN y = Student('Mickey','Mouse') THEN y.set_class('Freshman') THEN y.set_major('Electrical Engineering') THEN y.set_advisor(x) *** EXPECT: y.firstname+':'+y.lastname+':'+y.classyear+':'+y.major+':'+y.advisor.firstname+':'+y.advisor.lastname == 'Mickey:Mouse:Freshman:Electrical Engineering:Stephen:May' ***
-        """
-        x = Faculty('Stephen','May','Computer Science')
-        y = Student('Mickey','Mouse')
-        y.set_class('Freshman')
-        y.set_major('Electrical Engineering')
-        y.set_advisor(x)
-        self.assertEqual('Mickey:Mouse:Freshman:Electrical Engineering:Stephen:May', y.firstname+':'+y.lastname+':'+y.classyear+':'+y.major+':'+y.advisor.firstname+':'+y.advisor.lastname)
-        print()
-
-class Test06_StudentInheritsPerson(unittest.TestCase):
-    def test_list_int(self):
-        """
-        *** Test06 *** FUNCTION CALL: x = Faculty('Stephen','May','Computer Science') THEN y = Student('Mickey','Mouse') THEN y.set_class('Freshman') THEN y.set_major('Electrical Engineering') THEN y.set_advisor(x) *** EXPECT: isinstance(y, Person) == True ***
-        """
-        x = Faculty('Stephen','May','Computer Science')
-        y = Student('Mickey','Mouse')
-        y.set_class('Freshman')
-        y.set_major('Electrical Engineering')
-        y.set_advisor(x)
-        self.assertEqual(True, isinstance(y, Person))
-        print()
-
-class Test07_StudentInheritsFaculty(unittest.TestCase):
-    def test_list_int(self):
-        """
-        *** Test07 *** FUNCTION CALL: x = Faculty('Stephen','May','Computer Science') THEN y = Student('Mickey','Mouse') THEN y.set_class('Freshman') THEN y.set_major('Electrical Engineering') THEN y.set_advisor(x) *** EXPECT: isinstance(y, Faculty) == False ***
-        """
-        x = Faculty('Stephen','May','Computer Science')
-        y = Student('Mickey','Mouse')
-        y.set_class('Freshman')
-        y.set_major('Electrical Engineering')
-        y.set_advisor(x)
-        self.assertEqual(False, isinstance(y, Faculty))
-        print()
-
-
-if __name__ == '__main__':
-    with open('test.txt', "w") as f:
-        runner = unittest.TextTestRunner(f)
-        unittest.main(testRunner=runner)
+if __name__ == "__main__":
+    test_two_sum()
+    test_length_of_last_word()
+    test_search_insert()
